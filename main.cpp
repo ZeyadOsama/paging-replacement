74ed4bbd8ba80331b04ee5c5d396a120e2a9be40
ZeyadOsama <zeyad.osama@gmail.com>
2019-12-13 17:43:42 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+///
+/// main.c
+/// Paging and Replacement
+///
+/// @author Zeyad Osama.
+/// @date 2019-12-13.
+/// @copyright © 2019 Zeyad Osama. All rights reserved.
+///
+
+#include <iostream>
+
+int main() {
+    return 0;
+}
