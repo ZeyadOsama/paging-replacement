a17ee9ba9efb130ec92a83a7bbee6d1dab1db6c9
ZeyadOsama <zeyad.osama@gmail.com>
2019-12-31 16:34:48 +0200

    Enhancement

--- core/PageReplacer.cpp
+++ core/PageReplacer.cpp
@@ -7,33 +7,9 @@
 #include "PageReplacer.h"
 #include "LinkedList.h"
 
-#define CMD_CNT 4
-
 using namespace std;
 
-/**
- * @brief commands function.
- */
-void (*cmd_fn[])(LinkedList *l, TYPE v) = {
-        &_OPTIMAL,
-        &_FIFO,
-        &_LRU,
-        &_CLOCK
-};
-
-/**
- * @brief commands string.
- */
-string cmd_name[CMD_CNT];
-
-void initCommands(string *cmd) {
-    cmd[0] = "OPTIMAL";
-    cmd[1] = "FIFO";
-    cmd[2] = "LRU";
-    cmd[3] = "CLOCK";
-}
-
-void printRow(LinkedList *l, const int _pRef, const bool &_F) {
+void printRow(const LinkedList *l, const int _pRef, const bool &_F) {
     printf("%02d %c   ", _pRef, _F ? 'F' : ' ');
     Node *temp_ptr = l->head;
     while (temp_ptr != nullptr) {
@@ -43,40 +19,22 @@ void printRow(LinkedList *l, const int _pRef, const bool &_F) {
     cout << endl;
 }
 
-PageReplacer::PageReplacer(const ReplacementPolicy &_p, const int &_cntPages) {
-    policy = _p;
-    cntPages = _cntPages;
+PageReplacer::PageReplacer(const std::string &_policy, const int &_length) {
+    policy = _policy;
+    length = _length;
     initList(&list);
-    initCommands(cmd_name);
 }
 
 int PageReplacer::requestPage(const int &_pRef) {
-    if (search(&list, _pRef) == 1)
+    if (search(&list, _pRef))
         printRow(&list, _pRef, false);
-    else if (list.length < cntPages) {
+    else if (list.length < length) {
         add(&list, _pRef);
         printRow(&list, _pRef, false);
     } else {
-        for (int i = 0; i < CMD_CNT; i++)
-            if (toString(policy) == cmd_name[i]) {
-                (*cmd_fn[i])(&list, _pRef);
-                break;
-            }
+        _CMD(policy, &list, _pRef);
         printRow(&list, _pRef, true);
         return 1;
     }
     return 0;
-}
-
-string toString(ReplacementPolicy _p) {
-    string _policy;
-    if (_p == OPTIMAL)
-        _policy = "OPTIMAL";
-    else if (_p == FIFO)
-        _policy = "FIFO";
-    else if (_p == LRU)
-        _policy = "LRU";
-    else if (_p == CLOCK)
-        _policy = "CLOCK";
-    return _policy;
 }
